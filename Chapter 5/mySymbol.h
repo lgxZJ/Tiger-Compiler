337f8a6493fb9d6ce1ef27a9cf28c7ece0719885
lgxZJ <lgxZJ@outlook.com>
2016-10-13 18:58:57 +0800

    -retest mySymbol.c

---
+++ Chapter 5/mySymbol.h
@@ -0,0 +1,120 @@
+#ifndef MYSYMBOL_H
+#define MYSYMBOL_H
+
+/**
+ *	@defgroup	Symbol	Symbols
+ *	@{
+ **/
+
+/**
+ *	@file	mySymbol.h
+ *	@author	lgxZJ@outlook.com
+ *	@date	5/24/2016
+ *
+ *	@brief	Module about symbols.
+ */
+
+//
+//	Symbol module:
+//
+//	mySymbol is just a address of mySymbol_ structure
+//		containing symbol names of language source file
+//	By using this pointer it makes up a symbol list
+//
+
+#include "myTable.h"
+
+
+typedef char*	            myString;
+typedef struct mySymbol_*   mySymbol;
+
+
+
+/**
+ *	@brief  Check symbols' validity.
+ *	@param[in]	The symbol to be checked.
+ *	@return	If the symbol is valid, return true; Otherwise, return false.
+ *	@remark	Invalid symbols are NULLs.
+ */
+bool MySymbol_InvalidSymbol(mySymbol symbol);
+
+/**
+ *  @brief  Check two symbols' equality.
+ *  @param[in]  lhs One valid symbol.
+ *  @param[in]  rhs One valid symbol.
+ *  @return     If equals, return true; otherwise, return false;
+ */
+bool MySymbol_IsSymbolEqual(mySymbol lhs, mySymbol rhs);
+
+
+/**
+ *	@brief	Make s symbol from a string.
+ *
+ *	@param[in]	str	The string to make symbol from.
+ *
+ *	@return	If the string is NULL or empty, return \b NULL; If the string
+ *			is valid, return a symbol made from it.
+ *
+ *	@remark	When a new symbol is made, it is also added to the internal
+ *			symbol table to avoid remaking. If the symbol made is already
+ *			existed, this function simply returns it.
+ */
+mySymbol MySymbol_MakeSymbol(myString str);
+
+/**
+ *	@brief	Get the string name of the given symbol.
+ *	@param[in]	symbol	The symbol to extract name from.
+ *	@return	The extracted string name.
+ */
+myString MySymbol_GetName(const mySymbol symbol);
+
+/**
+ *	@brief	Make a new valid empty table to hold symbols.
+ *	@param	void	This function hsa no parameters.
+ *	@return	If the function succeeds, the return value is a new table.
+ *			If the function fails, the return value is \b NULL.
+ */
+myTable MySymbol_MakeNewTable();
+
+/**
+ *	@brief	Enter a \b symbol-value pair, namely a binding, into a table.
+ *
+ *	@param[in]	table	The table where to enter.
+ *	@param[in]	symbol	A valid symbol to enter.
+ *	@param[in]	value	The value to enter.
+ *
+ *	@return	This function has no return value.
+ */
+void MySymbol_Enter(myTable table, const mySymbol symbol, void* const value);
+
+/**
+ *	@brief	Look a symbol's most recent value in the given table.
+ *
+ *	@param[in]	table	The table where to look for.
+ *	@param[in]	symbol	The symbol to be looked.
+ *	@return	If the function successfully looked, return the symbol's value;
+ *			If the function fails, return \b NULL.
+ *
+ *	@todo	Change the return type.		
+ */
+void* MySymbol_Look(const myTable table, const mySymbol symbol);
+
+/**
+ *	@brief	Start a new scope in the given table.
+ *	@param[in]	table	The table where to start a new scope.
+ *	@return This function has no return value.
+ */
+void MySymbol_BeginScope(myTable table);
+
+/**
+ *	@brief	End the most recent scope in the given table.
+ *	@param[in]	table	The table where to end a scope.
+ *	@return	This function has no return value.
+ *
+ *	@remark	When a scope ends, all bindings inside are also removed.
+ */
+void MySymbol_EndScope(myTable table);
+
+/**	@}	*/
+
+#endif
---
+++ Chapter 5/tests/mySymbol/test_mySymbol.c
@@ -0,0 +1,269 @@
+#include "../testHelper.h"
+
+#include "../../mySymbol.h"
+
+//////////////////////////////////////////////////////////////////////
+
+void test_MySymbolInvalidSymbol_PassNull_ReturnTrue(void)
+{
+    mySymbol nullSymbol = NULL;
+
+    bool result = MySymbol_InvalidSymbol(nullSymbol);
+
+    CU_ASSERT_EQUAL(result, true);
+}
+
+void test_MySymbolInvalidSymbol_PassValid_ReturnFalse(void)
+{
+    mySymbol validSymbol = MySymbol_MakeSymbol("as");
+
+    bool result = MySymbol_InvalidSymbol(validSymbol);
+
+    CU_ASSERT_EQUAL(result, false);
+}
+
+/////////////////////////////////////////
+
+void test_MySymbolMakeSymbol_PassNullOrEmptyString_ReturnNull(void)
+{
+    myString nullString = NULL;
+    myString emptyString = "";
+
+    mySymbol nullResult = MySymbol_MakeSymbol(nullString);
+    mySymbol emptyResult = MySymbol_MakeSymbol(emptyString);
+
+    CU_ASSERT_EQUAL(nullResult, NULL);
+    CU_ASSERT_EQUAL(emptyResult, NULL);
+}
+
+void test_MySymbolMakeSymbol_PassValidString_MakeOne(void)
+{
+    myString validString = "valid";
+
+    mySymbol returnSymbol = MySymbol_MakeSymbol(validString);
+
+    CU_ASSERT_NOT_EQUAL(returnSymbol, NULL);
+}
+
+void test_MySymbolMakeSymbol_PassSameString_ReturnExisted(void)
+{
+    myString string = "string";
+    mySymbol existedSymbol = MySymbol_MakeSymbol(string);
+
+    mySymbol resultSymbol = MySymbol_MakeSymbol(string);
+
+    CU_ASSERT_EQUAL(resultSymbol, existedSymbol);
+}
+
+////////////////////////////////////////////////
+
+void test_MySymbolGetName_ByDefault_GetStringNameOfSymbol(void)
+{
+    myString stringMadeFrom = "string";
+    mySymbol symbol = MySymbol_MakeSymbol(stringMadeFrom);
+
+    myString stringGot = MySymbol_GetName(symbol);
+
+    CU_ASSERT_EQUAL(stringMadeFrom, stringGot);
+}
+
+/////////////////////////////////////////////////
+
+void test_MySymbolEnter_LegalParams_ReturnTrueAndAddOne(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbol = MySymbol_MakeSymbol("as");
+    void* valueEntered = (void*)121;
+
+    MySymbol_Enter(table, symbol, valueEntered);
+
+    void* valueGot = MySymbol_Look(table, symbol);
+    CU_ASSERT_EQUAL(valueGot, valueEntered);
+}
+
+///////////////////////////////////////////////////////////
+
+void test_MySymbolLook_PassSymbolNotEntered_ReturnNull(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol notEnteredSymbol = MySymbol_MakeSymbol("not entered");
+
+    void* result = MySymbol_Look(table, notEnteredSymbol);
+
+    CU_ASSERT_EQUAL(result, NULL);
+}
+
+void test_MySymbolLook_EnterSome_CanGetAny(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbolOne = MySymbol_MakeSymbol("one");
+    mySymbol symbolTwo = MySymbol_MakeSymbol("two");
+    void* valueOne = (void*)12;
+    void* valueTwo = (void*)34;
+    MySymbol_Enter(table, symbolOne, valueOne);
+    MySymbol_Enter(table, symbolTwo, valueTwo);
+
+    void* valueOneGot = MySymbol_Look(table, symbolOne);
+    void* valueTwoGot = MySymbol_Look(table, symbolTwo);
+
+    CU_ASSERT_EQUAL(valueOne, valueOneGot);
+    CU_ASSERT_EQUAL(valueTwo, valueTwoGot);
+}
+
+///////////////////////////////////////////////////////////
+
+void test_MySymbolBeginScope_AddSomeAfterScope_CanFindAny(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    MySymbol_BeginScope(table);
+    mySymbol symbolOne = MySymbol_MakeSymbol("one");
+    mySymbol symbolTwo = MySymbol_MakeSymbol("two");
+    void* valueOne = (void*)12;
+    void* valueTwo = (void*)23;
+    MySymbol_Enter(table, symbolOne, valueOne);
+    MySymbol_Enter(table, symbolTwo, valueTwo);
+
+    void* valueOneGot = MySymbol_Look(table, symbolOne);
+    void* valueTwoGot = MySymbol_Look(table, symbolTwo);
+
+    CU_ASSERT_EQUAL(valueOneGot, valueOne);
+    CU_ASSERT_EQUAL(valueTwoGot, valueTwo); 
+}
+
+void test_MySymbolBeginScope_AfterCalled_BindingsInsideFormerScopeCanBeFound(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbolBeforeScope = MySymbol_MakeSymbol("symbol");
+    void* value = (void*)112;
+    MySymbol_Enter(table, symbolBeforeScope, value);
+
+    MySymbol_BeginScope(table);
+
+    void* valueGot = MySymbol_Look(table, symbolBeforeScope);
+
+    CU_ASSERT_EQUAL(valueGot, value); 
+}
+
+void test_MySymbolBeginScope_EnterOneWithSameSymbolAfterCalled_OnlyFindLaterOne(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbol = MySymbol_MakeSymbol("symbol");
+    void* valueBefore = (void*)12;
+    void* valueAfter = (void*)23;
+    MySymbol_Enter(table, symbol, valueBefore);
+
+    MySymbol_BeginScope(table);
+    MySymbol_Enter(table, symbol, valueAfter);
+
+    void* valueGot = MySymbol_Look(table, symbol);
+    CU_ASSERT_EQUAL(valueGot, valueAfter);
+}
+
+void test_MySymbolBeginScope_BeginTwoScope_OnlyFindTheLastwithSameSymbol(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbol = MySymbol_MakeSymbol("symbol");
+    void* valueBefore = (void*)12;
+    void* valueAfter = (void*)23;
+    MySymbol_Enter(table, symbol, valueBefore);
+    MySymbol_BeginScope(table);
+    MySymbol_Enter(table, symbol, valueBefore);
+
+    MySymbol_BeginScope(table);
+    MySymbol_Enter(table, symbol, valueAfter);
+
+    void* valueGot = MySymbol_Look(table, symbol);
+    CU_ASSERT_EQUAL(valueGot, valueAfter);
+}
+
+////////////////////////////////////////////////////////////
+
+void test_MySymbolEndScope_AfterCalled_BindingsInThatScopeCanNotBeFound(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbolOne = MySymbol_MakeSymbol("symbolOne");
+    mySymbol symbolTwo = MySymbol_MakeSymbol("symbolTwo");
+    MySymbol_BeginScope(table);
+    MySymbol_Enter(table, symbolOne, (void*)12);
+    MySymbol_Enter(table, symbolTwo, (void*)23);
+
+    MySymbol_EndScope(table);
+
+    mySymbol valueOneGot = MySymbol_Look(table, symbolOne);
+    mySymbol valueTwoGot = MySymbol_Look(table, symbolTwo);
+    CU_ASSERT_EQUAL(valueOneGot, NULL);
+    CU_ASSERT_EQUAL(valueTwoGot, NULL);
+}
+
+void test_MySymbolEndScope_AfterCalled_SameBindingInFormerScopeCanBeFound(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol sameSymbol = MySymbol_MakeSymbol("symbol");
+    void* valueBefore = (void*)12;
+    void* valueAfter = (void*)34;
+    MySymbol_Enter(table, sameSymbol, valueBefore);
+    MySymbol_BeginScope(table);
+    MySymbol_Enter(table, sameSymbol, valueAfter);
+
+    MySymbol_EndScope(table);
+    void* valueGot = MySymbol_Look(table, sameSymbol);
+
+    CU_ASSERT_EQUAL(valueGot, valueBefore);
+}
+
+void test_MySymbolEndScope_CalledWithNoCorrespondingBeingScope_ActAsDefaultScope(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol symbolOne = MySymbol_MakeSymbol("symbolOne");
+    mySymbol symbolTwo = MySymbol_MakeSymbol("symbolTwo");
+    MySymbol_Enter(table, symbolOne, (void*)12);
+    MySymbol_Enter(table, symbolTwo, (void*)23);
+
+    MySymbol_EndScope(table);
+
+    mySymbol valueOneGot = MySymbol_Look(table, symbolOne);
+    mySymbol valueTwoGot = MySymbol_Look(table, symbolTwo);
+    CU_ASSERT_EQUAL(valueOneGot, NULL);
+    CU_ASSERT_EQUAL(valueTwoGot, NULL);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+int main ()
+{
+    CU_pSuite suite;
+    if (!initialTests(&suite))  return -1;
+
+    CU_TestInfo tests[] = {
+        { "", test_MySymbolInvalidSymbol_PassNull_ReturnTrue },
+        { "", test_MySymbolInvalidSymbol_PassValid_ReturnFalse },
+
+        { "", test_MySymbolMakeSymbol_PassNullOrEmptyString_ReturnNull },
+        { "", test_MySymbolMakeSymbol_PassValidString_MakeOne },
+        { "", test_MySymbolMakeSymbol_PassSameString_ReturnExisted },
+
+        { "", test_MySymbolGetName_ByDefault_GetStringNameOfSymbol },
+
+        { "", test_MySymbolEnter_LegalParams_ReturnTrueAndAddOne },
+
+        { "", test_MySymbolLook_PassSymbolNotEntered_ReturnNull },
+        { "", test_MySymbolLook_EnterSome_CanGetAny },
+
+        { "", test_MySymbolBeginScope_AfterCalled_BindingsInsideFormerScopeCanBeFound },
+        { "", test_MySymbolBeginScope_AddSomeAfterScope_CanFindAny },
+        { "", test_MySymbolBeginScope_EnterOneWithSameSymbolAfterCalled_OnlyFindLaterOne },
+        { "", test_MySymbolBeginScope_BeginTwoScope_OnlyFindTheLastwithSameSymbol },
+
+        { "", test_MySymbolEndScope_AfterCalled_BindingsInThatScopeCanNotBeFound },
+        { "", test_MySymbolEndScope_AfterCalled_SameBindingInFormerScopeCanBeFound },
+        { "", test_MySymbolEndScope_CalledWithNoCorrespondingBeingScope_ActAsDefaultScope }
+        
+    };
+    if (!addTests(&suite, tests, sizeof(tests) / sizeof(tests[0])))
+        return -1;
+
+    if (!runTests(&suite))  return -1;
+
+    freeMemoryChain();
+    return 0;
+}
