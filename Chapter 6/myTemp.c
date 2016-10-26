2d3e0131c2f16ef92477d21c069b31ce2b3702f3
lgxZJ <lgxZJ@outlook.com>
2016-10-26 14:27:27 +0800

    -add myTemp module and tests in chapter 6

---
+++ Chapter 6/myTemp.c
@@ -0,0 +1,59 @@
+#include "myTemp.h"
+#include "mySymbol.h"
+
+#include <stdio.h> //  for sprintf()
+#include <assert.h> //  for assert()
+
+//  keep track of myTemp and myLabel numbers
+
+struct myTemp_  { int num; };
+
+//////////////////////////////////////////////////////////////
+//                      global variables
+static int g_tempCount = 0;
+
+int Temp_getTotalTempCount(void)     { return g_tempCount; }
+
+
+static int g_labelCount = 0;
+
+int Temp_getTotalLabelCount(void)   { return g_labelCount; }
+
+
+/////////////////////////////////////////////////////////////
+//                      functions
+
+bool Temp_isLabelEqual(myLabel lhs, myLabel rhs)
+{
+    return MySymbol_IsSymbolEqual(lhs, rhs);
+}
+
+myTemp 	Temp_newTemp(void)
+{
+    myTemp temp = makeMemoryBlock(sizeof(*temp), MEMORY_TYPE_NONE);
+    assert (temp);
+
+    temp->num = ++g_tempCount;
+    return temp;
+}
+
+myLabel Temp_newLabel(void)
+{
+    char* buffer = makeMemoryBlock(sizeof(char) * 20, MEMORY_TYPE_NONE);
+    assert (buffer);
+
+    sprintf(buffer, "Label%d", ++g_labelCount);
+    return Temp_newNamedLabel(buffer);
+}
+
+//  NOTE:   try to call Temp_newLabel() instead, because it do not
+//  count correctly.
+myLabel Temp_newNamedLabel(myString name)
+{
+    return MySymbol_MakeSymbol(name);
+}
+
+myString Temp_getLabelString(myLabel label)
+{
+    return MySymbol_GetName(label);
+}
---
+++ Chapter 6/myTemp.h
@@ -0,0 +1,63 @@
+#ifndef MY_TEMP_H
+#define MY_TEMP_H
+
+#include "mySymbol.h"
+
+//  abstract names for local variables
+typedef struct myTemp_	*myTemp;
+//  abstract names for static memory addresses
+typedef mySymbol	    myLabel;
+
+/// \brief      compare two labels if they are equal.
+/// \param[in]  lhs     left operand.
+/// \param[in]  rhs     right operand.
+/// \return     a bool variable. 
+bool Temp_isLabelEqual(myLabel lhs, myLabel rhs);
+
+/**
+ *  @brief  make a new temporary in register representation.
+ *  @param  this function has no parameters.
+ *  @return a new temporary register representation.
+ */
+myTemp 	Temp_newTemp(void);
+
+/**
+ *  @brief  make a new label for function address.
+ *  @param  this function has no parameters.
+ *  @return a new label for function address.
+ */
+myLabel Temp_newLabel(void);
+
+///
+/// @brief      make a new label of the given name.
+/// @params[in] name    the name of the label.
+/// @return     a new label.
+/// @remark     if a label with the given name is already made, then
+///             this function simply returns that one. 
+myLabel Temp_newNamedLabel(myString name);
+
+myString Temp_getLabelString(myLabel label);
+
+////////////////////////
+
+typedef struct myTempList_
+{
+    myTemp 		head;
+    struct myTempList_*	tail;
+}* myTempList;
+
+myTempList Temp_makeTempList(myTemp head, myTempList tail);
+
+
+typedef struct myLabelList_
+{
+    myLabel			head;
+    struct myLabelList_*	tail;
+}* myLabelList;
+
+myLabelList Temp_makeLabelList(myLabel head, myLabelList tail);
+
+//////////////////////////////////////////////////////////////////
+//	more needed, but not now
+
+#endif
