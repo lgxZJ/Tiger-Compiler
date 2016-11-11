46f41f1258150a2128a6c6fc85a73e080a31e8ca
lgxZJ <lgxZJ@outlook.com>
2016-11-11 11:50:58 +0800

    -add a field getter from variable entry

---
+++ Chapter 7/myEnvironment.c
@@ -0,0 +1,286 @@
+#include "myEnvironment.h"
+#include "typeMaker.h"
+
+#include <assert.h> //  for assert()
+#include <stddef.h> //  for NULL definition
+
+//////////////////////////////////////////////////////////////
+
+struct myVarAndFuncEntry_
+{
+    enum EntryType { VarEntry, FuncEntry, EmptyEntry } kind;
+    union 
+    {
+        struct { Trans_myAccess access; myType type ;} varEntry;
+        struct  { Trans_myLevel level;      myLabel label;
+            myTypeList formalParamTypes;    myType returnType; } funcEntry;
+    }u;
+};
+
+///////////////////////////////////////////////////////////////
+
+myVarAndFuncEntry myEnvironment_makeVarEntry(
+    Trans_myAccess access,  myType type)
+{
+    myVarAndFuncEntry entry = 
+        makeMemoryBlock(sizeof(*entry), MEMORY_TYPE_NONE);
+    assert(entry);
+
+    entry->kind = VarEntry;
+    entry->u.varEntry.access = access;
+    entry->u.varEntry.type = type;
+
+    return entry;
+}
+
+myVarAndFuncEntry myEnvironment_makeFuncEntry(
+    Trans_myLevel level, myLabel label,
+    myTypeList formalParamTypes, myType returnType)
+{
+    myVarAndFuncEntry entry =
+        makeMemoryBlock(sizeof(*entry), MEMORY_TYPE_NONE);
+    assert(entry);
+
+    entry->kind = FuncEntry;
+    entry->u.funcEntry.level = level;
+    entry->u.funcEntry.label = label;
+    entry->u.funcEntry.formalParamTypes = formalParamTypes;
+    entry->u.funcEntry.returnType = returnType;
+
+    return entry;
+}
+
+bool myEnvironment_isVarEntry(myVarAndFuncEntry entry)
+{
+    return entry->kind == VarEntry; 
+}
+
+bool myEnvironment_isFuncEntry(myVarAndFuncEntry entry)   
+{
+    return entry->kind == FuncEntry; 
+}
+
+///////////////////////////////////////////////////////////
+
+void MyEnvironment_addNewVarOrFunc(
+    myTable varAndFuncEnv, mySymbol entryName, myVarAndFuncEntry newEntry)
+{
+    MySymbol_Enter(varAndFuncEnv, entryName, newEntry);
+}
+
+void MyEnvironment_addNewType(
+    myTable typeEnv, mySymbol typeName, myType newType)
+{
+    MySymbol_Enter(typeEnv, typeName, newType);
+}
+
+//  NOTE:   entry must has been already added
+myVarAndFuncEntry MyEnvironment_getVarOrFuncFromName(
+    myTable varAndFuncEnv, mySymbol varOrFuncName)
+{
+    return (myVarAndFuncEntry)MySymbol_Look(varAndFuncEnv, varOrFuncName);
+}
+
+myType MyEnvironment_getVarType(myVarAndFuncEntry varEntry)
+{
+    assert (varEntry);
+    assert (myEnvironment_isVarEntry(varEntry));
+    return varEntry->u.varEntry.type;
+}
+
+/////////////////////////////////////////////////////////
+
+//  NOTE:   type must has been already added
+myType MyEnvironment_getTypeFromName(
+    myTable typeEnv, mySymbol typeName)
+{
+    return (myType)MySymbol_Look(typeEnv, typeName);
+}
+
+//  NOTE:  The parameter must ba a valid function entry.
+myTypeList MyEnvironment_getFuncFormalTypes(myVarAndFuncEntry funcEntry)
+{
+    assert (funcEntry);
+    assert (myEnvironment_isFuncEntry(funcEntry));
+    return funcEntry->u.funcEntry.formalParamTypes;
+}
+
+//  NOTE:  The parameter must ba a valid function entry.
+myType MyEnvironment_getFuncReturnType(myVarAndFuncEntry funcEntry)
+{
+    assert (funcEntry);
+    assert (myEnvironment_isFuncEntry(funcEntry));
+    return funcEntry->u.funcEntry.returnType;
+}
+
+Trans_myLevel MyEnvironment_getFuncLevel(myVarAndFuncEntry funcEntry)
+{
+    assert (funcEntry);
+    assert (myEnvironment_isFuncEntry(funcEntry));
+    return funcEntry->u.funcEntry.level;
+}
+
+myLabel MyEnvironment_getFuncLabel(myVarAndFuncEntry funcEntry)
+{
+    assert (funcEntry);
+    assert (myEnvironment_isFuncEntry(funcEntry));
+    return funcEntry->u.funcEntry.label;
+}
+
+Trans_myAccess MyEnvironment_getVarAccess(myVarAndFuncEntry varEntry)
+{
+    assert (varEntry);
+    assert(myEnvironment_isVarEntry(varEntry));
+    return varEntry->u.varEntry.access;
+}
+
+////////////////////////////////////////////////////////////
+
+bool MyEnvironment_updateTypeValue(
+    myTable typeEnv, mySymbol typeName, myType newType)
+{
+    assert (typeEnv && newType);
+    assert (!MySymbol_InvalidSymbol(typeName));
+    return MySymbol_Set(typeEnv, typeName, newType);
+}
+
+void MyEnvironment_updateFuncLevel(
+    myTable varAndFuncEnv, mySymbol funcName, Trans_myLevel level)
+{
+    assert (varAndFuncEnv && level);
+    assert (!MySymbol_InvalidSymbol(funcName));
+
+    myVarAndFuncEntry entry = MySymbol_Look(varAndFuncEnv, funcName);
+    assert (entry && myEnvironment_isFuncEntry(entry));
+
+    entry->u.funcEntry.level = level;
+}
+
+void MyEnvironment_updateFuncLabel(
+    myTable varAndFuncEnv, mySymbol funcName, myLabel label)
+{
+    assert (varAndFuncEnv && label);
+    assert (!MySymbol_InvalidSymbol(funcName));
+
+    myVarAndFuncEntry entry = MySymbol_Look(varAndFuncEnv, funcName);
+    assert (entry && myEnvironment_isFuncEntry(entry));
+
+    entry->u.funcEntry.label = label;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/* base environment where contains predefiend functions in tiger language */
+myTable myEnvironment_BaseVarAndFunc(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    assert (table);
+
+    //  add `print` Func
+    myVarAndFuncEntry entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("print"),
+        makeType_TypeList(makeType_String(), NULL),
+        makeType_NoReturn());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("print"), entry);
+
+    //  add `flush` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("flush"),
+        makeType_TypeList(NULL, NULL),
+        makeType_NoReturn());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("flush"), entry);
+
+
+    //  add `getchar` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("getchar"),
+        makeType_TypeList(NULL, NULL),
+        makeType_String());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("getchar"), entry);
+
+    //  add `ord` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("ord"),
+        makeType_TypeList(makeType_String(), NULL),
+        makeType_Int());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("ord"), entry);
+
+    //  add `chr` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("chr"),
+        makeType_TypeList(makeType_Int(), NULL),
+        makeType_String());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("chr"), entry);
+
+    //  add `size` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("size"),
+        makeType_TypeList(makeType_String(), NULL),
+        makeType_Int());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("size"), entry);
+
+    //  add `substring` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("substring"),
+        makeType_TypeList(
+            makeType_String(), 
+            makeType_TypeList(
+                makeType_Int(), 
+                makeType_TypeList(makeType_Int(), NULL))),
+        makeType_String());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("substring"), entry);
+
+    //  add `concat` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("concat"),
+        makeType_TypeList(
+            makeType_String(),
+            makeType_TypeList(
+                makeType_String(),
+                NULL)),
+        makeType_String());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("concat"), entry);
+
+    //  add `not` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("not"),
+        makeType_TypeList(makeType_Int(), NULL),
+        makeType_Int());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("not"), entry);
+
+    //  add `exit` Func
+    entry = myEnvironment_makeFuncEntry(
+        Trans_outermostLevel(),
+        Temp_newNamedLabel("exit"),
+        makeType_TypeList(makeType_Int(), NULL),
+        makeType_NoReturn());
+    MySymbol_Enter(table, MySymbol_MakeSymbol("exit"), entry);
+
+    return table;
+}
+
+
+myTable myEnvironment_BaseType(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    assert(table != NULL);
+
+    myType type = makeType_Int();
+
+    MySymbol_Enter(table, MySymbol_MakeSymbol("int"), type);
+
+    type = makeType_String();
+    MySymbol_Enter(table, MySymbol_MakeSymbol("string"), type);
+
+    return table;
+}
---
+++ Chapter 7/tests/myEnvironment/test_myEnvironment.c
@@ -0,0 +1,179 @@
+#include "../testHelper.h"
+
+#include "../../mySymbol.h"
+#include "../../makeMemory.h"
+#include "../../myEnvironment.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+/////////////////////////////////////////////////////////////////////////////
+//                              fake functions
+myTable MySemantic_getVarAndFuncEnvironment(void)
+{
+    return MySymbol_MakeNewTable();
+}
+
+void test_makeVarEntry_ByDefault_MakeOneValidVarEntry(void)
+{
+    Trans_myAccess fakeVarAccess = (Trans_myAccess)12;
+    void* fakeVarType = (void*)23;
+
+    myVarAndFuncEntry entry = myEnvironment_makeVarEntry(
+        fakeVarAccess,fakeVarType);
+
+    CU_ASSERT(entry != NULL);
+    CU_ASSERT(myEnvironment_isVarEntry(entry));
+    CU_ASSERT_EQUAL(MyEnvironment_getVarType(entry), fakeVarType);
+    CU_ASSERT_EQUAL(MyEnvironment_getVarAccess(entry), fakeVarAccess);
+}
+
+void test_makeFuncEntry_ByDefault_MakeOneValidFuncEntry(void)
+{
+    void* fakeFormalTypes = NULL;
+    void* fakeReturnType = NULL;
+
+    myVarAndFuncEntry entry = myEnvironment_makeFuncEntry(
+        (Trans_myLevel)NULL, (myLabel)NULL,
+        fakeFormalTypes, fakeReturnType);
+
+    CU_ASSERT(entry != NULL);
+    CU_ASSERT(myEnvironment_isFuncEntry(entry));
+    CU_ASSERT_EQUAL(MyEnvironment_getFuncFormalTypes(entry), fakeFormalTypes);
+    CU_ASSERT_EQUAL(MyEnvironment_getFuncReturnType(entry), fakeReturnType);
+}
+
+void test_addNewVarOrFunc_ByDefault_AddOne(void)
+{
+    myTable varAndFuncEnv = myEnvironment_BaseVarAndFunc();
+    myType fakeType = NULL;
+    mySymbol entryName = MySymbol_MakeSymbol("entryName");
+    myVarAndFuncEntry entry = myEnvironment_makeVarEntry(
+        (Trans_myAccess)NULL, fakeType);
+
+    MyEnvironment_addNewVarOrFunc(varAndFuncEnv, entryName, entry);
+
+    myVarAndFuncEntry actualEntry = 
+        MyEnvironment_getVarOrFuncFromName(varAndFuncEnv, entryName); 
+    CU_ASSERT_EQUAL(entry, actualEntry);
+}
+
+void test_addNewType_ByDefault_AddOne(void)
+{
+    myTable typeEnv = myEnvironment_BaseType();
+
+    myType expectedType = NULL;
+    mySymbol typeName = MySymbol_MakeSymbol("typename");
+
+    MyEnvironment_addNewType(typeEnv, typeName, expectedType);
+
+    myType actualType = MyEnvironment_getTypeFromName(typeEnv, typeName); 
+    CU_ASSERT_EQUAL(expectedType, actualType);
+}
+
+void test_MyEnvironmentupdateTypeValue_ValidSet_ReturnTrueAndTheOneSetted(void)
+{
+    myTable typeEnv = myEnvironment_BaseType();
+
+    myType formerType = NULL;
+    mySymbol typeName = MySymbol_MakeSymbol("typename");
+    MyEnvironment_addNewType(typeEnv, typeName, formerType);
+
+    myType typeSetted = (myType)12;
+    bool result =
+        MyEnvironment_updateTypeValue(typeEnv, typeName, typeSetted);
+
+    myType typeGot = MyEnvironment_getTypeFromName(typeEnv, typeName);
+    CU_ASSERT_EQUAL(result, true);
+    CU_ASSERT_EQUAL(typeGot, typeSetted);
+}
+
+void test_myEnvironmentBaseVarAndFunc_BeDefault_ContainPredefinedFuncs(void)
+{
+    char*   funcNames[] = {
+        "print", "flush", "getchar", "ord", "chr",
+        "size", "substring", "concat", "not", "exit"
+    };
+
+    myTable table = myEnvironment_BaseVarAndFunc();
+    for (int i = 0; i < sizeof(funcNames) / sizeof(funcNames[0]); ++i)
+    {
+        void* result = MySymbol_Look(table, MySymbol_MakeSymbol(*funcNames));
+        CU_ASSERT_NOT_EQUAL (result, NULL);
+    }
+}
+
+void test_myEnvironmentBaseType_BeDefault_ContainsIntuitiveTypes(void)
+{
+    char* typeNames[] = { "int", "string" };
+
+    myTable table = myEnvironment_BaseType();
+    for (int i = 0; i < sizeof(typeNames) / sizeof(typeNames[0]); ++i)
+    {
+        void* result = MySymbol_Look(table, MySymbol_MakeSymbol(typeNames[i]));
+        CU_ASSERT_NOT_EQUAL (result, NULL);
+    }
+}
+
+void test_MyEnvironmentUpdateFuncLevel_ByDefault_GetWhatUpdated(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol funcName = MySymbol_MakeSymbol("asas");
+    myVarAndFuncEntry funcEntry = myEnvironment_makeFuncEntry(
+        (Trans_myLevel)NULL, (myLabel)NULL,
+        (myTypeList)NULL, (myType)NULL);
+    MyEnvironment_addNewVarOrFunc(table, funcName, funcEntry);
+
+    Trans_myLevel fakeLevel = (Trans_myLevel)12;
+    MyEnvironment_updateFuncLevel(table, funcName, fakeLevel);
+
+    Trans_myLevel levelGot = MyEnvironment_getFuncLevel(
+        MyEnvironment_getVarOrFuncFromName(table, funcName));
+    CU_ASSERT_EQUAL(levelGot, fakeLevel);
+}
+
+void test_MyEnvironmentUpdateFuncLabel_ByDefault_GetWhatUpdated(void)
+{
+    myTable table = MySymbol_MakeNewTable();
+    mySymbol funcName = MySymbol_MakeSymbol("asas");
+    myVarAndFuncEntry funcEntry = myEnvironment_makeFuncEntry(
+        (Trans_myLevel)NULL, (myLabel)NULL,
+        (myTypeList)NULL, (myType)NULL);
+    MyEnvironment_addNewVarOrFunc(table, funcName, funcEntry);
+
+    myLabel fakeLabel = (myLabel)12;
+    MyEnvironment_updateFuncLabel(table, funcName, fakeLabel);
+
+    myLabel labelGot = MyEnvironment_getFuncLabel(
+        MyEnvironment_getVarOrFuncFromName(table, funcName));
+    CU_ASSERT_EQUAL(labelGot, fakeLabel);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+int main (int argc, char* argv[])
+{
+    CU_pSuite suite;
+    if (!initialTests(&suite))  return -1;
+
+    //	add tests
+    CU_TestInfo tests[] = {
+        { "", test_makeVarEntry_ByDefault_MakeOneValidVarEntry },
+        { "", test_makeFuncEntry_ByDefault_MakeOneValidFuncEntry },
+        { "", test_addNewVarOrFunc_ByDefault_AddOne },
+        { "", test_addNewType_ByDefault_AddOne },
+        { "", test_MyEnvironmentupdateTypeValue_ValidSet_ReturnTrueAndTheOneSetted },
+        { "", test_myEnvironmentBaseVarAndFunc_BeDefault_ContainPredefinedFuncs },
+        { "", test_myEnvironmentBaseType_BeDefault_ContainsIntuitiveTypes },
+        { "", test_MyEnvironmentUpdateFuncLevel_ByDefault_GetWhatUpdated },
+        { "", test_MyEnvironmentUpdateFuncLabel_ByDefault_GetWhatUpdated }
+    };
+    if (!addTests(&suite, tests, sizeof(tests) / sizeof(tests[0])))
+        return -1;
+    
+    if (!runTests(&suite))
+        return -1;
+
+    freeMemoryChain();
+    return 0;
+}
