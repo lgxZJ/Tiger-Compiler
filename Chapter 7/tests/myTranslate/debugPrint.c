d9685d4565f6d87cce720952d712a066d62169cc
lgxZJ <lgxZJ@outlook.com>
2016-11-18 11:19:10 +0800

    -refactor statement and expression printers

--- Chapter 7/tests/myTranslate/debugPrint.c
+++ Chapter 7/tests/myTranslate/debugPrint.c
@@ -24,158 +24,202 @@ static void newline(FILE* file)
 }
 
 ////////////////////////////////////////////
+
+//  forwards
 void printExpression(FILE* file, IR_myExp exp, int spaceNum);
 
 //////
+
+void indentAndPrintToFile(FILE* file, int spaceNum, char* str)
+{
+    indentSpace(file, spaceNum);
+    fprintf(file, "%s",  str);
+}
+
+void printNull(FILE* file)
+{
+    fprintf(file, "null");
+}
+
+void printSeqStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "Seq(");
+    printStatement(file, statement->u.seq.left, spaceNum + 2),
+
+    indentAndPrintToFile(file, spaceNum, ", ");
+    printStatement(file, statement->u.seq.right, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printCJumpStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "CJump(%s, ", g_relOperatorStr[statement->u.cjump.op]);
+    printExpression(file,  statement->u.cjump.left, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ", ");
+    printExpression(file,  statement->u.cjump.right, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ", ");
+    fprintf(file, "%s, ", MySymbol_GetName(statement->u.cjump.trueLabel));
+    fprintf(file, "%s, ", MySymbol_GetName(statement->u.cjump.falseLabel));
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printExpStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "Exp(");
+    printExpression(file, statement->u.exp, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printJumpStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "Jump(");
+    printExpression(file, statement->u.jump.exp, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printLabelStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "Label(%s)\n", MySymbol_GetName(statement->u.label));
+}
+
+void printMoveStatement(FILE* file, IR_myStatement statement, int spaceNum)
+{
+    fprintf(file, "Move(");
+    printExpression(file, statement->u.move.dst, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ", ");
+    printExpression(file, statement->u.move.src, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
 
 void printStatement(FILE* file, IR_myStatement statement, int spaceNum)
 {
     newline(file), indentSpace(file, spaceNum);
 
     if (statement == NULL) 
-    {
-        fprintf(file, "null");
-        return;
-    }
+        return printNull(file);
 
     switch (statement->kind)
     {
         case IR_Seq:
-            fprintf(file, "Seq(");
-            printStatement(file, statement->u.seq.left, spaceNum + 2),
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-            printStatement(file, statement->u.seq.right, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break; 
+            return printSeqStatement(file, statement, spaceNum);
         case IR_CJump:
-            fprintf(file, "CJump(%s, ", g_relOperatorStr[statement->u.cjump.op]);
-            printExpression(file,  statement->u.cjump.left, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-
-            printExpression(file,  statement->u.cjump.right, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-
-            fprintf(file, "%s, ", MySymbol_GetName(statement->u.cjump.trueLabel));
-            fprintf(file, "%s, ", MySymbol_GetName(statement->u.cjump.falseLabel));
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printCJumpStatement(file, statement, spaceNum);
         case IR_Exp:
-            fprintf(file, "Exp(");
-            printExpression(file, statement->u.exp, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printExpStatement(file, statement, spaceNum);
         case IR_Jump:
-            fprintf(file, "Jump(");
-            printExpression(file, statement->u.jump.exp, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printJumpStatement(file, statement, spaceNum);
         case IR_Label:
-            fprintf(file, "Label(%s)\n", MySymbol_GetName(statement->u.label));
-            break;
+            return printLabelStatement(file, statement, spaceNum);
         case IR_Move:
-            fprintf(file, "Move(");
-            printExpression(file, statement->u.move.dst, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-            printExpression(file, statement->u.move.src, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printMoveStatement(file, statement, spaceNum);
         default:        assert (false);
     }
+}
+
+//////////////////////////////////////////////////////
+
+void printBinOperation(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "BinOperation(%s, ", g_binOperatorStr[exp->u.binOperation.op]);
+    printExpression(file, exp->u.binOperation.left, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ", ");
+    printExpression(file, exp->u.binOperation.right, spaceNum + 2);
+
+    indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printCall(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "Call(");
+	printExpression(file, exp->u.call.func, spaceNum + 2);
+
+	IR_myExpList exps = exp->u.call.args;
+	while (exps)
+	{
+	    indentAndPrintToFile(file, spaceNum, ", ");
+	    printExpression(file, exps->head, spaceNum + 2);
+
+	    exps = exps->tails;
+	}
+
+	indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printConst(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "Const(%d)\n", exp->u.constValue);
+}
+
+void printESeq(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "Eseq(");
+	printStatement(file, exp->u.eseq.statement, spaceNum + 2);
+
+	indentAndPrintToFile(file, spaceNum, ", ");
+	printExpression(file, exp->u.eseq.exp, spaceNum + 2);
+
+	indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printMem(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "Mem(");
+	printExpression(file, exp->u.mem, spaceNum + 2);
+
+	indentAndPrintToFile(file, spaceNum, ")\n");
+}
+
+void printName(FILE* file, IR_myExp exp, int spaceNum)
+{
+    fprintf(file, "Name(%s)\n", MySymbol_GetName(exp->u.name));
+}
+
+void printTemp(FILE* file, IR_myExp exp, int spaceNum)
+{
+    if (exp->u.temp == Frame_FP())
+	    fprintf(file, "Temp(%s)\n", "frame pointer");
+	else if (exp->u.temp == Frame_RV())
+        fprintf(file, "Temp(%s)\n", "return register");
+	else
+        fprintf(file, "Temp(%d)\n", Temp_getTempNum(exp->u.temp));
 }
 
 void printExpression(FILE* file, IR_myExp exp, int spaceNum)
 {
     newline(file), indentSpace(file, spaceNum);
     if (exp == NULL)
-    {
-        fprintf(file, "null");
-        return;
-    }
+        return printNull(file);
     
     switch (exp->kind)
     {
         case IR_BinOperation:
-            fprintf(file, "BinOperation(%s, ", g_binOperatorStr[exp->u.binOperation.op]);
-            printExpression(file, exp->u.binOperation.left, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-            printExpression(file, exp->u.binOperation.right, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printBinOperation(file, exp, spaceNum);
         case IR_Call:
-            fprintf(file, "Call(");
-            printExpression(file, exp->u.call.func, spaceNum + 2);
-
-            IR_myExpList exps = exp->u.call.args;
-            while (exps)
-            {
-                indentSpace(file, spaceNum);
-                fprintf(file, ", ");
-                printExpression(file, exps->head, spaceNum + 2);
-                exps = exps->tails;
-            }
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printCall(file, exp, spaceNum);
         case IR_Const:
-            fprintf(file, "Const(%d)\n", exp->u.constValue);
-            break;
+            return printConst(file, exp, spaceNum);
         case IR_ESeq:
-            fprintf(file, "Eseq(");
-            printStatement(file, exp->u.eseq.statement, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ", ");
-            printExpression(file, exp->u.eseq.exp, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");;
-            break;
+            return printESeq(file, exp, spaceNum);
         case IR_Mem:
-            fprintf(file, "Mem(");
-            printExpression(file, exp->u.mem, spaceNum + 2);
-
-            indentSpace(file, spaceNum);
-            fprintf(file, ")\n");
-            break;
+            return printMem(file, exp, spaceNum);
         case IR_Name:
-            fprintf(file, "Name(%s)\n", MySymbol_GetName(exp->u.name));
-            break;
+            return printName(file, exp, spaceNum);
         case IR_Temp:
-        {
-            if (exp->u.temp == Frame_FP())
-                fprintf(file, "Temp(%s)\n", "frame pointer");
-            else if (exp->u.temp == Frame_RV())
-                fprintf(file, "Temp(%s)\n", "return register");
-            else
-                fprintf(file, "Temp(%d)\n", Temp_getTempNum(exp->u.temp));
-            break;
-        }
+            return printTemp(file, exp, spaceNum);
         default:    assert (false);
     }
 }
+
+////////////////////////////////////////////////////////////
 
 void printFragments(FILE* file, Frame_myFragList fragments)
 {
