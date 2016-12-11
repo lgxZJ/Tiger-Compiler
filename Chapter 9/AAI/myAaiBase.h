7f5f0d36082d70dc76a93523143f612a7236071a
lgxZJ <lgxZJ@outlook.com>
2016-12-12 05:26:27 +0800

    -add AAI base header

---
+++ Chapter 9/AAI/myAaiBase.h
@@ -0,0 +1,44 @@
+#ifndef MY_AAI_H
+#define MY_AAI_H
+
+#include "../CXX_myTemp.h"
+#include <string>
+
+namespace lgxZJ
+{
+    namespace IS    //  stand for Instruction Selection phase
+    {
+        ///////////////////////////////////////////////////////////////////////
+        //  NOTE:
+        //      Almost every instructions in X86 have 5 syntax:
+        //          1.Register  -   Register
+        //          2.Register  -   Memory
+        //          3.Register  -   ConstValue
+        //          4.Memory    -   Register
+        //          5.Memory    -   ConstValue
+        //      But, only some of them are implemented in this module because
+        //      not all of them are needed in tiger.
+        ///////////////////////////////////////////////////////////////////////
+
+        struct BinaryUnion
+        {
+            enum Kind { Reg, Value }    kind;
+            union{   myTemp reg; int value; }u;
+        };
+
+		class AAI
+		{
+		    public:
+		        virtual std::string ToString() const = 0;
+
+                //  Default definition
+                virtual myTempList TrashedRegs() const { return nullptr; }
+		};
+
+        class Controlable : public AAI  {};
+        class Computable :  public AAI  {};
+        class Movable :     public AAI  {};
+	}
+}
+
+#endif
