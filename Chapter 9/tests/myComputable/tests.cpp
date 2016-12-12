8b6a838610c0a30b57139558c3e33084ad73a6c8
lgxZJ <lgxZJ@outlook.com>
2016-12-12 17:24:11 +0800

    -remove test fixtures into outer files

--- Chapter 9/tests/myComputable/tests.cpp
+++ Chapter 9/tests/myComputable/tests.cpp
@@ -5,30 +5,9 @@
 
 #include "../../AAI/myComputable.h"
 
+#include "../TestFixture.h"
+
 using namespace lgxZJ::IS;
-
-template <typename T>
-class TwoComputeMocker : public T
-{
-    public:
-        explicit TwoComputeMocker(T t) : T(t)    {}
-
-        myTemp GetDstReg()  { return T::dstReg; }
-        myTemp GetSrcReg()  { return T::srcRep.u.reg; }
-        int    GetSrcValue(){ return T::srcRep.u.value; }
-        BinaryUnion::Kind GetSrcType() { return T::srcRep.kind; }
-};
-
-template <typename T>
-class OneComputeMocker : public T
-{
-    public:
-        explicit OneComputeMocker(T t) : T(t)    {}
-
-        myTemp GetSrcReg()  { return T::srcRep.u.reg; }
-        int    GetSrcValue(){ return T::srcRep.u.value; }
-        BinaryUnion::Kind GetSrcType() { return T::srcRep.kind; }
-};
 
 class TwoComputeTest : public CppUnit::TestFixture
 {
@@ -64,58 +43,7 @@ class TwoComputeTest : public CppUnit::TestFixture
                 
             return suite;
         }
-
-        ///////////////////////////////////////////////////////////////////////
-        //                          tests template
-
-        template <typename T>
-        void testTwoOperandCtor_BothOperandsReg_SetWhatPassed()
-        {
-            myTemp dst = Temp_newTemp();
-            myTemp src = Temp_newTemp();
-            T add(dst, src);
-
-            TwoComputeMocker<T> addMocker(add);
-            CPPUNIT_ASSERT_EQUAL(dst, addMocker.GetDstReg());
-            CPPUNIT_ASSERT_EQUAL(BinaryUnion::Kind::Reg, addMocker.GetSrcType());
-            CPPUNIT_ASSERT_EQUAL(src, addMocker.GetSrcReg());
-        }
-
-        template <typename T>
-        void testTwoOperandCtor_LeftRegRightConstValue_SetWhatPassed()
-        {
-            myTemp dst = Temp_newTemp();
-            int srcValue = 1;
-            T one(dst, srcValue);
-
-            TwoComputeMocker<T> oneMocker(one);
-            CPPUNIT_ASSERT_EQUAL(dst, oneMocker.GetDstReg());
-            CPPUNIT_ASSERT_EQUAL(BinaryUnion::Kind::Value, oneMocker.GetSrcType());
-            CPPUNIT_ASSERT_EQUAL(srcValue, oneMocker.GetSrcValue());
-        }
-
-        template <typename T>
-        void testOneOperandCtor_RegSource_SetWhatPassed()
-        {
-            myTemp src = Temp_newTemp();
-            T one(src);
-
-            OneComputeMocker<T> oneMocker(one);
-            CPPUNIT_ASSERT_EQUAL(BinaryUnion::Kind::Reg, oneMocker.GetSrcType());
-            CPPUNIT_ASSERT_EQUAL(src, oneMocker.GetSrcReg());
-        }
-
-        template <typename T>
-        void testOneOperandCtor_ValueSource_SetWhatPassed()
-        {
-            int srcValue;
-            T one(srcValue);
-
-            OneComputeMocker<T> oneMocker(one);
-            CPPUNIT_ASSERT_EQUAL(BinaryUnion::Kind::Value, oneMocker.GetSrcType());
-            CPPUNIT_ASSERT_EQUAL(srcValue, oneMocker.GetSrcValue());
-        }
-
+        
         ///////////////////////////////////////////////////////////////////////
         //                              tests
 
