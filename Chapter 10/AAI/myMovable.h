3cfe117d2040a71747cefd8854589865e14235bf
lgxZJ <lgxZJ@outlook.com>
2016-12-15 18:58:18 +0800

    -add GetDstReg() and GetSrcReg() interface to myMovable module

---
+++ Chapter 10/AAI/myMovable.h
@@ -0,0 +1,45 @@
+#ifndef MY_MOVABLE_H
+#define MY_MOVABLE_H
+
+#include "myAaiBase.h"
+
+namespace lgxZJ
+{
+    namespace IS
+    {
+        //  Here we use on class to represent abstract assembly language instruction.
+        //  So, we need to know when to treat as register values and when as memory
+        //  locations pointed to by registers. However, there are too many conditions.
+        //  To make things simple, i just use enumerators rather than more subclass to
+        //  differ them.
+        class Move : public AAI
+        {
+            public:
+                //  Content -> Register
+                //  Memory  -> [Register]
+                enum OperandType { Content, Memory, None };
+
+                explicit Move(myTemp oneDstReg, myTemp oneSrcReg,
+                            OperandType oneDstType = Content,
+                            OperandType oneSrcType = Content);
+                explicit Move(myTemp oneDstReg, OperandType oneDstType, int constValue);
+
+                virtual myTemp GetDstReg() const;
+                virtual myTemp GetSrcReg() const;
+
+                virtual std::string ToString() const;
+
+            private:
+                std::string OneOperandToString(myTemp operand, OperandType type) const;
+
+            protected:
+                myTemp      dstReg;
+                OperandType dstType;
+
+                BinaryUnion srcRep;
+                OperandType srcType;
+        };
+    }
+}
+
+#endif
