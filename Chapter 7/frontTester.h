c9c22eb4a63f892bae77292eb6a07a04be41fef7
lgxZJ <lgxZJ@outlook.com>
2017-01-04 17:04:26 +0800

    -add a front end tester into the library

---
+++ Chapter 7/frontTester.h
@@ -0,0 +1,13 @@
+#ifndef MY_FRONT_TESTER_H
+#define MY_FRONT_TESTER_H
+
+#include "myIRTree.h"
+
+/**
+ *  @brief	Do the front tests for a given source file.
+ *  @remark filename must be a valid filename.
+ */
+IR_myStatement frontTester(char* filename);
+
+#endif
+
