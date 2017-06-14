63a6e57d853eefe6c508b0947e1d595c5c2738c6
lgxZJ <lgxZJ@outlook.com>
2017-06-14 11:21:21 +0800

    -tests for If-Then passed

--- Chapter 12/main.cpp
+++ Chapter 12/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {   
-    for (int i = 1; i <= 14; ++i)
+    for (int i = 1; i <= 15; ++i)
     {
         if (i == 2 || i == 12) continue;   //  todo:test2.tig and test12.tig needs rewrite
 
