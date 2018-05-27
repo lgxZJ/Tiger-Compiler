cca0c4415d9f6b3e2ae3224b9411ebdcc1576bc5
lgxZJ <lgxZJ@outlook.com>
2018-05-27 16:26:55 +0800

    + add an install script for CentOS

--- travis_scripts/install_centos.sh
+++ travis_scripts/install_centos.sh
@@ -3,3 +3,9 @@ sudo yum install gcc-c++
 # used for static linking
 sudo yum install libstdc++-static
 sudo yum install glibc-static
+
+chmod u+x ./install_cppunit_centos.sh
+./install_cppunit_centos.sh
+
+chmod u+x ./install_glic-devel-i686_centos.sh
+./install_glic-devel-i686_centos.sh
