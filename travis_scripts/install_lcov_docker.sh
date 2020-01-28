6f9198e56989a7daa9e47a6f90b4132adc8b7c20
lgxZJ <lgxzj@outlook.com>
2020-01-28 10:46:01 +0000

    + fix lcov dependencies

--- travis_scripts/install_lcov_docker.sh
+++ travis_scripts/install_lcov_docker.sh
@@ -1,3 +1,10 @@
+# install lcov
 wget http://downloads.sourceforge.net/ltp/lcov-1.13-1.noarch.rpm
 rpm -Uvh lcov-1.13-1.noarch.rpm
 rm -f lcov-1.13-1.noarch.rpm
+
+# install lcov deps
+yum install -y perl-Digest-MD5
+
+# install genhtml deps
+yum install -y perl-GD
